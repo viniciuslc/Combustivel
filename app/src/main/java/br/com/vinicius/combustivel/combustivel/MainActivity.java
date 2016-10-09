087c48b66a2bb674fe64eea60836ead910e06e3f
Vinicius <viniciuslc@gmail.com>
2016-10-09 17:18:32 -0300

    - Novo Layout, nova toolbar, itens de menu e configurações.

--- app/src/main/java/br/com/vinicius/combustivel/combustivel/MainActivity.java
+++ app/src/main/java/br/com/vinicius/combustivel/combustivel/MainActivity.java
@@ -2,6 +2,10 @@ package br.com.vinicius.combustivel.combustivel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Adicionando a toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                Toast.makeText(getApplicationContext(), "Clicou em configurações", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_novo:
+                Toast.makeText(getApplicationContext(), "Clicou em Novo", Toast.LENGTH_SHORT).show();
+                return true;
+
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
+
